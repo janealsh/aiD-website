0c2764d347aa6b06e70c3b7b1adb6a0e0a1ea741
janealsh <janealshihabi@gmail.com>
2026-01-26 17:29:36 -0500

    create and use preliminary header component

---
+++ src/lib/ui/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '../../routes/layout.css';
+	let { lastCommit, author } = $props();
+</script>
+
+<div class="header navbar text-gray-200 shadow-sm">
+	<div class="navbar-start">
+		<span>v0.1</span>
+	</div>
+	<div class="navbar-center">
+		<span>ai:D</span>
+	</div>
+	<div class="navbar-end">
+		<span>Last commit {lastCommit} ago by {author}</span>
+	</div>
+</div>
+
+<style>
+	.header {
+		font-family: 'Milling';
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { SvelteFlow, Background, BackgroundVariant } from '@xyflow/svelte';
 	import Node from '$lib/ui/Node.svelte';
+	import Header from '$lib/ui/Header.svelte';
 	import '@xyflow/svelte/dist/style.css';
 </script>
 
@@ -10,6 +11,7 @@
 
 <div class="grid-bg"></div>
 
+<Header lastCommit="2 days ago" author="CamillaS" />
 <div class="main">
 	<Node
 		nodeText="The Norwegian Centre on AI for Decisions (aiD) is a premier research hub dedicated to advancing the role of artificial intelligence in complex decision-making processes. As a cornerstone of the Research Council of Norway’s (RCN) AI portfolio, aiD bridges technological, organizational, and human-centric gaps to foster a society where AI-driven value creation is safe and ethical."
@@ -27,7 +29,7 @@
 	.grid-bg {
 		position: fixed;
 		inset: 0;
-        background-color: #050505;
+		background-color: #050505;
 		background-image: radial-gradient(#3f3f3f 1px, transparent 1px);
 		background-size: 24px 24px;
 		z-index: -1;
