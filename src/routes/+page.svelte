fafd50d4d1efebd6d331732d7101e4e2899a1337
janealsh <janealshihabi@gmail.com>
2026-01-26 17:10:38 -0500

    replace background and add preliminary Node component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { SvelteFlow, Background, BackgroundVariant } from '@xyflow/svelte';
+	import Node from '$lib/ui/Node.svelte';
 	import '@xyflow/svelte/dist/style.css';
 </script>
 
@@ -7,14 +8,28 @@
 	<title>AID Home</title>
 </svelte:head>
 
-<div style:width="100vw" style:height="100vh">
-	<SvelteFlow maxZoom={1} minZoom={1}>
-		<Background
-			bgColor="#0A0A0A"
-			patternColor="#959595"
-			variant={BackgroundVariant.Cross}
-            size={12}
-			gap={200}
-		/>
-	</SvelteFlow>
+<div class="grid-bg"></div>
+
+<div class="main">
+	<Node
+		nodeText="The Norwegian Centre on AI for Decisions (aiD) is a premier research hub dedicated to advancing the role of artificial intelligence in complex decision-making processes. As a cornerstone of the Research Council of Norway’s (RCN) AI portfolio, aiD bridges technological, organizational, and human-centric gaps to foster a society where AI-driven value creation is safe and ethical."
+	/>
 </div>
+
+<style>
+	.main {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 200;
+		right: 0;
+	}
+	.grid-bg {
+		position: fixed;
+		inset: 0;
+        background-color: #050505;
+		background-image: radial-gradient(#3f3f3f 1px, transparent 1px);
+		background-size: 24px 24px;
+		z-index: -1;
+	}
+</style>
