bd40a3516b12bd13cefa2a197e8c47fcbb49e0f1
janealsh <janealshihabi@gmail.com>
2026-01-28 12:16:43 -0500

    update tab icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/aiD_profile_picture_black.png';
 
 	let { children } = $props();
 </script>
